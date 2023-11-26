3d7b95962d8c37132a1646a3bc437ef95d323b5f
mojitosharma <mohit20086@iiitd.ac.in>
2023-11-26 23:06:51 +0530

    Dummy data update

--- backend/populate_Data_for_testing.sql
+++ backend/populate_Data_for_testing.sql
@@ -52,3 +52,20 @@ VALUES
 
 DELETE FROM server_meetings;
 DELETE FROM server_Attendance;
+
+
+DELETE FROM server_FormStatus;
+INSERT INTO server_FormStatus ("formId", "formStatus")
+VALUES
+    ('1', '1'),
+	('2', '1'),
+	('3', '1');
+
+DELETE FROM server_meetings;
+INSERT INTO server_meetings ("meetingId", "schedulerId", "title", "date", "time", "attendee", "description", "mentorBranches")
+VALUES
+    ('1', '101', 'SetByMentor1', '2023-11-09', '12:30', 2, 'Test Meeting from mentorId=1', '[]'),
+    ('2', '105', 'SetByMentor5', '2023-12-12', '01:30', 2, 'Test Meeting from mentorId=5', '[]'),
+    ('3', '1', 'SetForMentors', '2023-11-10', '02:30', 1, 'Test Meeting from AdminId=1','["B-CSB","B-CSAI"]'),
+    ('4', '1', 'SetForMentees', '2023-12-11', '03:30', 2, 'Test Meeting from AdminId=1', '[]'),
+    ('5', '1', 'SetForBoth', '2023-12-11', '04:30', 3, 'Test Meeting from AdminId=1','["B-CSB","B-CSAI"]');
