a46831a648b898dcedcebc7344315e30f7637973
mojitosharma <mohit20086@iiitd.ac.in>
2024-02-16 21:26:40 +0530

    mentor mentee mapping done

--- backend/server/views.py
+++ backend/server/views.py
@@ -833,35 +833,55 @@ def update_attendance(request):
 def create_mentor_mentee_pairs(request):
     if request.method == "POST":
         try:
+            data = json.loads(request.body.decode('utf-8'))
+            subject = data.get('subject')
+            message = data.get('body')
+            candidate_ids = data.get('Id', [])
+
+            if not all([subject, message, candidate_ids]):
+                return JsonResponse({"error": "Missing required data"}, status=400)
+
             departments = Mentee.objects.values_list('department', flat=True).distinct()
-            noMenteeBranch = []
+            department_dict = {department: [] for department in departments}
+
+            emails = []
+            for candidate_id in candidate_ids:
+                candidate = Candidate.objects.get(id=candidate_id)
+                candidate.status = '5'
+                department_dict[candidate.department].append(candidate)
+                candidate.save()
+                emails.append(candidate.email)
+
             for department in departments:
                 mentees = Mentee.objects.filter(department=department, mentorId='')
-                mentee_batch_size = math.ceil(len(mentees) / 5.0)
-                candidates = Candidate.objects.filter(status=3, department=department).order_by('-score')[:mentee_batch_size]  
+                candidates = department_dict[department]
                 candidates_dict = {candidate.id: 0 for candidate in candidates}
-                if len(candidates_dict) == mentee_batch_size:
-                    for mentee in mentees:
-                        candidate_id = random.choice([key for key, value in candidates_dict.items() if value < 5])
-                        mentee.mentorId = candidate_id
-                        mentee.save()
-                        candidates_dict[candidate_id] += 1
-                    
-                    for candidate in candidates:
-                        mentor = Mentor(id=candidate.id, goodiesStatus = 0)
-                        mentor.save()
-                        candidate.status = 5
-                        candidate.save()
-                else: 
-                    noMenteeBranch.append(department)
-            if len(noMenteeBranch):
-                return JsonResponse({'message': 'Not enough canidate for branches : '+ str(noMenteeBranch)})
-            else:
-                return JsonResponse({'message': "Mentor-Mentee Mapping is completed!"})
+                if len(candidates_dict) == 0: 
+                    continue
+                print("here 4")
+                mentee_batch_size = math.ceil(len(mentees) / len(candidates_dict))
+                for mentee in mentees:
+                    candidate_id = random.choice([key for key, value in candidates_dict.items() if value < mentee_batch_size])
+                    mentee.mentorId = candidate_id
+                    mentee.save()
+                    candidates_dict[candidate_id] += 1
+                
+                for candidate in candidates:
+                    mentor = Mentor(id=candidate.id, goodiesStatus = 0)
+                    mentor.save()
+                    candidate.status = 5
+                    candidate.save()
+
+            thread = threading.Thread(target=send_emails_to, args=(subject, message, settings.EMAIL_HOST_USER, emails))
+            thread.start()
+
+            return JsonResponse({'message': "Mail sent successfully"})
         except Exception as e:
+            print(e)
             return JsonResponse({'message': str(e)})
     else:
-        return JsonResponse({"message": "Invalid request method"})
+        return JsonResponse({"error": "Invalid request method"}, status=400)
+
 
 # Done   
 @csrf_exempt
