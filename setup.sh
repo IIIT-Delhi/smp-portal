4d08d6335a713eb8ad771151b03dcc71c5828600
mojitosharma <mohit20086@iiitd.ac.in>
2024-04-26 18:34:45 +0530

    setup updated

--- setup.sh
+++ setup.sh
@@ -9,17 +9,16 @@ sudo pip3 install virtualenv
 
 # Set up virtual environment
 virtualenv myenv
-sudo python3 -m venv sdos
-source sdos/bin/activate
+sudo apt update 
+sudo apt install python3-pip python3-dev nginx
 
-# Install Python dependencies
-pip install -r requirements.txt
+
 
 # Install Node.js
-curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
-sudo apt-get install -y nodejs
+sudo apt install nodejs npm
+npm install
 
-# Install system dependencies
+#Install prostresql
 sudo apt-get install -y postgresql
 
 # Create PostgreSQL database and user
@@ -28,6 +27,16 @@ sudo -u postgres psql -c "CREATE USER postgres WITH PASSWORD '12345';"
 sudo -u postgres psql -c "ALTER ROLE postgres SET client_encoding TO 'utf8';"
 sudo -u postgres psql -c "ALTER ROLE postgres SET timezone TO 'UTC';"
 sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE mydatabase TO postgres;"
+
+# Install system dependencies
+sudo python3 -m venv sdos
+source sdos/bin/activate
+
+# Install Python dependencies
+pip install -r requirements.txt
+pip install django gunicorn
+
+sudo ufw allow 8000
 
 # Move to the directory and start the models
 cd backend
