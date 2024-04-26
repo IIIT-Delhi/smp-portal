49a80772646b1cb1494ae15d4cd6672a81dcb695
mojitosharma <mohit20086@iiitd.ac.in>
2024-04-26 22:09:15 +0530

    setup

--- setup.sh
+++ setup.sh
@@ -2,17 +2,10 @@
 
 # Install Python and pip
 sudo apt-get update
-sudo apt-get install -y python3 python3-pip
+sudo apt-get install -y python3=3.11.5 python3-pip
 
-# Install virtualenv
-sudo pip3 install virtualenv
-
-# Set up virtual environment
-virtualenv myenv
-sudo apt update 
+# dowload enginx
 sudo apt install python3-pip python3-dev nginx
-
-
 
 # Install Node.js
 sudo apt install nodejs npm
@@ -22,21 +15,32 @@ npm install
 sudo apt-get install -y postgresql
 
 # Create PostgreSQL database and user
+apt-get install -y postgresql
 sudo -u postgres psql -c "CREATE DATABASE smp;"
-sudo -u postgres psql -c "CREATE USER postgres WITH PASSWORD '12345';"
-sudo -u postgres psql -c "ALTER ROLE postgres SET client_encoding TO 'utf8';"
-sudo -u postgres psql -c "ALTER ROLE postgres SET timezone TO 'UTC';"
-sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE mydatabase TO postgres;"
+sudo -u postgres psql -c "CREATE USER admin WITH PASSWORD '12345';"
+sudo -u postgres psql -c "ALTER ROLE admin SET client_encoding TO 'utf8';"
+sudo -u postgres psql -c "ALTER ROLE admin SET timezone TO 'UTC';"
+sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE smp TO admin;"
 
-# Install system dependencies
-sudo python3 -m venv sdos
-source sdos/bin/activate
 
-# Install Python dependencies
-pip install -r requirements.txt
-pip install django gunicorn
 
 sudo ufw allow 8000
+
+# Install virtualenv
+pip3 install virtualenv
+# Install system dependencies
+python3 -m venv sdos
+cd sdos
+sudo chown -R iiitd:iiitd /home/iiitd/smp/smp-portal/sdos
+cd ..
+source sdos/bin/activate
+
+
+# Install Python dependencies
+pip3 install typing-extensions sqlparse asgiref django
+pip3 install django gunicorn
+pip3 install pytz typing-extensions sqlparse psycopg2 psutil asgiref Django==4.2.5 djangorestframework==3.14.0 django-cors-headers==4.3.0
+
 
 # Move to the directory and start the models
 cd backend
